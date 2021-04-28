e884df72035373019b3cbb296a78fa1acf04da57
Alejandro Miranda <alejandromsz@hotmail.com>
2021-04-28 14:06:23 -0500

    Microservicio usuario funcional

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,27 +1,14 @@
-/* Populate tabla clientes */
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Alvaro', 'Salazar', 'alvaro@denkitronik.com', '2021-01-01');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', '2021-01-02');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2021-01-03');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2021-01-04');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2021-02-01');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2021-02-10');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2021-02-18');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2021-02-28');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', '2021-03-03');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'magma.lee@gmail.com', '2021-03-04');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'tornado.roe@gmail.com', '2021-03-05');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2021-03-06');
+
 
 INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('JadeDoe', '123', 'Administrador','Jade', 'Doe', 'jane.doe@gmail.com', '2021-03-06');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Alvaro', 'Salazar', 'Administrador','Alvaro', 'Salazar', 'alvaro@denkitronik.com', '2021-01-01');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Mr. John', 'Doe', 'Cliente','Mr. John', 'Doe', 'john.doe@gmail.com', '2021-01-02');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'Cliente','Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2021-01-03');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'Cliente','Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2021-01-04');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'Cliente', 'Erich', 'Gamma', 'erich.gamma@gmail.com', '2021-02-01');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'Cliente','Richard', 'Helm', 'richard.helm@gmail.com', '2021-02-10');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'Cliente', 'Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2021-02-18');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('John', 'Vlissides', 'Cliente','John', 'Vlissides', 'john.vlissides@gmail.com', '2021-02-28');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Dr. James', 'Gosling', 'Cliente','Dr. James', 'Gosling', 'james.gosling@gmail.com', '2021-03-03');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'Cliente', 'Magma', 'Lee', 'magma.lee@gmail.com', '2021-03-04');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'Cliente', 'Tornado', 'Roe', 'tornado.roe@gmail.com', '2021-03-05');
-INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'Cliente','Jade', 'Doe', 'jane.doe@gmail.com', '2021-03-06');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Alvaro123', 'Salazar123', 'Administrador','Alvaro', 'Salazar', 'alvaro@denkitronik.com', '2021-01-01');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Mr. John123', 'Doe123', 'Cliente','Mr. John', 'Doe', 'john.doe@gmail.com', '2021-01-02');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Linus123', 'Torvalds123', 'Cliente','Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2021-01-03');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Rasmus123', 'Lerdorf123', 'Cliente','Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2021-01-04');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Erich123', 'Gamma123', 'Cliente', 'Erich', 'Gamma', 'erich.gamma@gmail.com', '2021-02-01');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Richard123', 'Helm123', 'Cliente','Richard', 'Helm', 'richard.helm@gmail.com', '2021-02-10');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Ralph123', 'Johnson123', 'Cliente', 'Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2021-02-18');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('John123', 'Vlissides123', 'Cliente','John', 'Vlissides', 'john.vlissides@gmail.com', '2021-02-28');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Dr. James123', 'Gosling123', 'Cliente','Dr. James', 'Gosling', 'james.gosling@gmail.com', '2021-03-03');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Magma123', 'Lee123', 'Cliente', 'Magma', 'Lee', 'magma.lee@gmail.com', '2021-03-04');
+INSERT INTO usuarios (usuario, password, rol, nombre, apellido, email, create_at) VALUES('Tornado123', 'Roe123', 'Cliente', 'Tornado', 'Roe', 'tornado.roe@gmail.com', '2021-03-05');
